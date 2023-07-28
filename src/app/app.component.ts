fbde495dbc8797c50c0f5616e7b6a4ea74e98320
navodit <navodit50@gmail.com>
2023-07-28 19:06:55 +0530

    Initial commit - adding project files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'sunnySideAgencyLanding';
+export class AppComponent implements OnInit {
+  width!: number;
+  mobileView!: boolean;
+  menu = false;
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.width = screen.width;
+    // console.log('width : ', this.width);
+    this.mobileView = this.width < 679;
+    // console.log('mobileView : ', this.mobileView);
+    console.log(screen.width);
+  }
+  ngOnInit(): void {
+    this.onResize();
+  }
+  onClickMenu = () => {
+    this.menu = !this.menu;
+  };
 }
